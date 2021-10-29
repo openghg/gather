f3a1b14ccc39b7147b014cc56bcb90070b7202aa
Gareth Jones <oxidising@gmail.com>
2021-10-29 16:57:44 +0100

    Adding API key checking to web scraping function, can now be made available

--- functions/scrape/route.py
+++ functions/scrape/route.py
@@ -21,13 +21,25 @@ async def handle_invocation(ctx: InvokeContext, data: BytesIO) -> Response:
     Returns:
         dict: Dictionary of return data
     """
+    headers = ctx.Headers()
+
     try:
         # There's no point doing all the processing just find we
         # don't have a token
         _ = os.environ["GIT_TOKEN"]
+        # Get the authorization headers
+        auth_key = headers["authorization"]
+        # Read valid keys JSON from env variable
+        key_data = json.loads(os.environ["VALID_KEYS"])
+        valid_keys = key_data["keys"]
+        # Read args
         post_data = json.loads(data.getvalue())
     except Exception:
         error_str = str(format_exc())
+        return Response(ctx=ctx, response_data=error_str)
+
+    if auth_key not in valid_keys:
+        ctx.SetResponseHeaders({"Authorisation": "Denied"}, 403)
         return Response(ctx=ctx, response_data=error_str)
 
     # This handles the calls to each of the network pipelines
