f8b02edfea2227b41d77185c6303f6cf35a75d8b
Gareth Jones <oxidising@gmail.com>
2021-11-02 10:39:49 +0000

    WIP: Started adding some fixtures

--- tests/aqmesh/test_scraper.py
+++ tests/aqmesh/test_scraper.py
@@ -1,1 +1,16 @@
 from webscrape.aqmesh import scrape_data
+from requests_mock import ANY
+# from pandas import Timestamp
+from pathlib import Path
+
+@pytest.fixture(scope="session")
+def mock_return():
+    return {"some": "json"}
+
+def test_retrieve_met(requests_mock):
+    test_data_path = Path("../data/aqmesh/test_data.zip")
+    binary_data = test_data_path.read_bytes()
+
+    requests_mock.get(ANY, content=binary_data, status_code=200)
+
+    scrape_data()
--- webscrape/utils/__init__.py
+++ webscrape/utils/__init__.py
@@ -2,3 +2,5 @@ from ._download import download
 from ._checks import is_nan, is_date
 from ._loaders import load_json
 from ._combine import combine_networks
+
+__all__ = ["download", "is_nan", "is_date", "load_json", "combine_networks"]
