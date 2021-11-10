c95907db5033c551ca95f4766f3d1d283fe82fd3
Gareth Jones <oxidising@gmail.com>
2021-11-10 17:11:50 +0000

    Updated pipeline tests with mocks

--- tests/pipeline/test_beaco2n.py
+++ tests/pipeline/test_beaco2n.py
@@ -1,55 +1,58 @@
+import json
+from types import MethodDescriptorType
 from gather.pipeline import run_beaco2n
 from pathlib import Path
 from openghg.objectstore import get_local_bucket
 
+import gather.pipeline._beaco2n as _beaco2n_pipeline
 
-def test_beaco2n_pipeline(beaco2n_intercept, tmpdir):
-    get_local_bucket(empty=True)
+
+def test_beaco2n_pipeline(tmpdir, monkeypatch):
+    def mock_scrape(metadata, download_path):
+        return {m: f"/tmp/this/{m}" for m in metadata}
+
+    def mock_process(filepaths, metadata):
+        return None
+
+    def mock_export(sites, selected_vars):
+        return {
+            "network": {
+                "species": {
+                    "hillparksecondaryschool": {
+                        "data": {"2021-01-01": 432},
+                        "metadata": {"location": "glasgow"},
+                    }
+                }
+            }
+        }
+
+    monkeypatch.setattr(_beaco2n_pipeline, "scrape_data", mock_scrape)
+    monkeypatch.setattr(_beaco2n_pipeline, "process_pipeline", mock_process)
+    monkeypatch.setattr(_beaco2n_pipeline, "export_pipeline", mock_export)
 
     download_directory = Path(str(tmpdir))
 
     json_data = run_beaco2n(selected_vars=["co2"], download_path=download_directory)
 
-    expected_data = {
-        "beaco2n": {
-            "co2": {
-                "bellahoustonacademy": {
-                    "data": {"co2": {"1635382800000": 447.6, "1635393600000": 476.6}},
+    expected = {
+        "network": {
+            "species": {
+                "hillparksecondaryschool": {
+                    "data": {"2021-01-01": 432},
                     "metadata": {
-                        "units": "ppm",
-                        "site": "bellahoustonacademy",
-                        "species": "co2",
-                        "inlet": "na",
-                        "network": "beaco2n",
-                        "sampling_period": "not_set",
-                        "data_type": "timeseries",
-                        "deployed": "2021-07-15",
-                        "id": 175,
-                        "latitude": 55.848,
-                        "long_name": "Bellahouston Academy",
-                        "longitude": -4.301,
+                        "location": "glasgow",
+                        "deployed": "2021-07-28",
+                        "id": 174,
+                        "latitude": 55.815,
+                        "long_name": "Hillpark Secondary School",
+                        "longitude": -4.299,
                         "magl": "NA",
                         "masl": "NA",
-                        "node_folder_id": 932,
+                        "node_folder_id": 931,
                     },
                 }
             }
         }
     }
 
-    filenames = sorted([f.stem for f in download_directory.glob("*.csv")])
-
-    assert filenames == [
-        "156_killearnstirlingshireglasgows22002",
-        "157_glasgows12002",
-        "171_universityofstrathclyde",
-        "172_stpaulshighschool",
-        "174_hillparksecondaryschool",
-        "175_bellahoustonacademy",
-        "178_stthomasaquinasrcsecondaryschool",
-        "179_johnpaulacademy",
-        "193_knightswoodsecondary",
-        "197_notredamehighschool",
-    ]
-
-    assert json_data == expected_data
+    assert json_data == expected
